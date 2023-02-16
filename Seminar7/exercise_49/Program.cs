845b64a0ad4910e2822b577022567421d15eba49
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-16 15:04:51 +0300

    tasks had been acomplished

---
+++ Seminar7/exercise_49/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] MultiDimensionalArray()
+{
+    int[,] Array = new int[new Random().Next(8), new Random().Next(8)];
+    return Array;
+}
+
+int[,] RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(10);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] ModiffiedArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            if (i % 2 != 0 && j % 2 != 0) Array[i, j] = Array[i, j] * Array[i, j];
+        }
+    }
+    return Array;
+}
+
+int[,] Array = RandomFillArray(MultiDimensionalArray());
+PrintArray(Array);
+
+int[,] NewArray = ModiffiedArray(Array);
+Console.WriteLine();
+PrintArray(Array);
---
+++ Seminar7/exercise_51/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] MultiDimensionalArray()
+{
+    int[,] Array = new int[new Random().Next(8), new Random().Next(8)];
+    return Array;
+}
+
+int[,] RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(10);
+        }
+    }
+    return Array;
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int ComputeMainDiagonal(int[,] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            if (i == j) sum = sum + Array[i,j];
+        }
+    }
+    return sum;
+}
+
+
+int[,] Array = RandomFillArray(MultiDimensionalArray());
+PrintArray(Array);
+
+int sum = ComputeMainDiagonal(Array);
+Console.WriteLine(sum);
